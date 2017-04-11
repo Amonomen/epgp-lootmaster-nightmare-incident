48afd47ef65b06c4c6bb5eea87b7e3646fb2ccb1
Brett Spillman <bspillman91@gmail.com>
2017-04-11 10:20:14 -0400

    Initial git commit

---
+++ locale/esES.lua
@@ -0,0 +1,8 @@
+-- EPGPLootmaster Locale
+-- Please use the Localization App on Curseforge to Update this
+-- http://wow.curseforge.com/addons/epgp_lootmaster/localization/
+
+local L = LibStub("AceLocale-3.0"):NewLocale("EPGPLootmaster", "esES")
+if not L then return end
+
+--@localization(locale="esES", escape-non-ascii=true, format="lua_additive_table", same-key-is-true=true)@
---
+++ locale/zhCN.lua
@@ -0,0 +1,8 @@
+-- EPGPLootmaster Locale
+-- Please use the Localization App on Curseforge to Update this
+-- http://wow.curseforge.com/addons/epgp_lootmaster/localization/
+
+local L = LibStub("AceLocale-3.0"):NewLocale("EPGPLootmaster", "zhCN")
+if not L then return end
+
+--@localization(locale="zhCN", escape-non-ascii=true, format="lua_additive_table", same-key-is-true=true)@
